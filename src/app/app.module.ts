5cdd030d08421906498b1cce9f776cdd1958bdfb
JulsCode <jcah549@gmail.com>
2022-12-16 10:52:52 -0600

    implementando el footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,16 @@ import { AppComponent } from './app.component';
 import { EmpresasComponent } from './componentes/empresas/empresas.component';
 import { EmpresaService } from './componentes/empresas/empresa.service';
 import { HeaderComponent } from './componentes/header/header.component';
+import { ReqsComponent } from './componentes/reqs/reqs.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmpresasComponent,
-    HeaderComponent
+    HeaderComponent,
+    ReqsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/componentes/reqs/reqs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reqs',
+  templateUrl: './reqs.component.html',
+  styleUrls: ['./reqs.component.css']
+})
+export class ReqsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
